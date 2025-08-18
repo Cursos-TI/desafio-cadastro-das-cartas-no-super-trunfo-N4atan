d9a0306c2e787b14cbf8470c971e33f071d9744d
Natan <162522588+N4atan@users.noreply.github.com>
2025-08-18 14:23:55 +0000

    Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,8 @@ int main() {
     float c1_km       , c2_km;
     float c1_pib      , c2_pib;
     int   c1_pt       , c2_pt;
+    float c1_dp       , c2_dp;
+    float c1_pibC     , c2_pibC; //Necessário uma validação, se PIB é milhões ou bilhões.
 
     printf("\n ----- Cadastro de Carta 1 ----- \n");
     printf("\n 1.1 - De qual estado ela é?              [ A - H ]: ");
@@ -25,19 +27,42 @@ int main() {
     printf("\n 1.3 - Escolha um código:       [ 1 - 4 ]: ");
     scanf("%d", &c1_id);
 
-    printf("\n 1.4 - Qual a população estimada?         [ Ex.: xxxxx s/ \".\"]: ");
+    printf("\n 1.4 - Qual a população estimada?         [ Ex.: xxxxx s/ \".\" ]: ");
     scanf("%d", &c1_pop);
 
-    printf("\n 1.5 - Qual a area total estimada?        [ Ex.: xxxxx s/ \".\"]: ");
+    printf("\n 1.5 - Qual a area (km²) total estimada?        [ Ex.: xxxxx ]: ");
     scanf("%f", &c1_km);
 
-    printf("\n 1.6 - Qual o PIB estimado?               [ Ex.: xxxxx s/ \".\"]: ");
+    printf("\n 1.6 - Qual o PIB estimado?               [ Ex.: xxxxx ]: ");
     scanf("%f", &c1_pib);
 
     printf("\n 1.7 - Quantos pontos turisticos possui? ");
     scanf("%d", &c1_pt);
 
-    //---
+    c1_dp   = (float) c1_pop / c1_km;
+    c1_pibC = (float) c1_pib / c1_pop;
+
+    printf(
+        "\n ----- Exibindo Carta 1 ----- \n"
+        "-> Código: %c%d \n"
+        "-> Estado: %c\n"
+        "-> Nome: %s \n"
+        "-> Populacao: %d \n"
+        "-> Area: %.2fkm² \n"
+        "-> PIB: %.2f  \n"
+        "-> Qtd Pontos Turisticos: %d  \n"
+        "-> Densidade Populacional: %.2f hab/km² \n"
+        "-> PIB per Capita: %.2f  \n",
+        c1_uf, c1_id,
+        c1_uf,
+        c1_nome,
+        c1_pop,
+        c1_km,
+        c1_pib,
+        c1_pt,
+        c1_dp,
+        c1_pibC
+    );
 
     printf("\n ----- Cadastro de Carta 2 ----- \n");
     printf("\n 2.1 - De qual estado ela é?              [ A - H ]: ");
@@ -52,53 +77,38 @@ int main() {
     printf("\n 2.4 - Qual a população estimada?         [ Ex.: xxxxx s/ \".\"]: ");
     scanf("%d", &c2_pop);
 
-    printf("\n 2.5 - Qual a area total estimada?        [ Ex.: xxxxx s/ \".\"]: ");
+    printf("\n 2.5 - Qual a area (km²) total estimada?        [ Ex.: xxxxx ]: ");
     scanf("%f", &c2_km);
 
-    printf("\n 2.6 - Qual o PIB estimado?               [ Ex.: xxxxx s/ \".\"]: ");
+    printf("\n 2.6 - Qual o PIB estimado?               [ Ex.: xxxxx s/ ]: ");
     scanf("%f", &c2_pib);
 
     printf("\n 2.7 - Quantos pontos turisticos possui? ");
     scanf("%d", &c2_pt);
-    
-    //---
 
-    printf(
-        "\n ----- Exibindo Carta 1 ----- \n"
-        "-> Código: %d \n"
-        "-> Estado: %c\n"
-        "-> Nome: %s \n"
-        "-> Populacao: %d \n"
-        "-> Area: %f \n"
-        "-> PIB: %f  \n"
-        "-> Qtd Pontos Turisticos: %d  \n",
-        c1_id,
-        c1_uf,
-        c1_nome,
-        c1_pop,
-        c1_km,
-        c1_pib,
-        c1_pt
-    );
-    
-    //---
+    c2_dp   = (float) c2_pop / c2_km;
+    c2_pibC = (float) c2_pib / c2_pop;
 
     printf(
         "\n ----- Exibindo Carta 2 ----- \n"
-        "-> Código: %d \n"
+        "-> Código: %c%d \n"
         "-> Estado: %c\n"
         "-> Nome: %s \n"
         "-> Populacao: %d \n"
-        "-> Area: %f \n"
-        "-> PIB: %f  \n"
-        "-> Qtd Pontos Turisticos: %d  \n",
-        c2_id,
+        "-> Area: %.2fkm² \n"
+        "-> PIB: %.2f  \n"
+        "-> Qtd Pontos Turisticos: %d  \n"
+        "-> Densidade Populacional: %.2f hab/km² \n"
+        "-> PIB per Capita: %.2f  \n",
+        c2_uf, c2_id,
         c2_uf,
         c2_nome,
         c2_pop,
         c2_km,
         c2_pib,
-        c2_pt
+        c2_pt,
+        c2_dp,
+        c2_pibC
     );
 
     return 0;
